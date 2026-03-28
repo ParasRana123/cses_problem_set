6ed80062b1b768b4d8ea0168031599aed12618c2
ParasRana123 <parasrana579@gmail.com>
2026-03-28 18:28:10 +0530

    Updated: CSES DP coin combination 2 prob

--- DP/coin_combo_2.cpp
+++ DP/coin_combo_2.cpp
@@ -3,27 +3,17 @@ using namespace std;
 
 static const int MOD = 1e9 + 7;
 
-long long f(int ind , int sum , vector<vector<long long>> &dp , vector<int> &c) {
-    if(sum == 0) return 1;
-    if(ind == c.size()) return 0;
-    if(dp[ind][sum] != -1) return dp[ind][sum];
-    long long take = 0;
-    if(sum - c[ind] >= 0) {
-        take = f(ind , sum - c[ind] , dp , c);
-    }
-    long long notTake = f(ind + 1 , sum , dp , c);
-    return dp[ind][sum] = take + notTake;
-}
-
 int main() {
-    int n , x;
+    int n, x;
     cin >> n >> x;
-    vector<int> c(n);
-    for(int i = 0 ; i < n ; i++) {
-        cin >> c[i];
+    vector<int> coins(n);
+    for(int i = 0; i < n; i++) cin >> coins[i];
+    vector<int> dp(x + 1, 0);
+    dp[0] = 1;
+    for(int coin : coins) {
+        for(int sum = coin; sum <= x; sum++) {
+            dp[sum] = (dp[sum] + dp[sum - coin]) % MOD;
+        }
     }
-    vector<vector<long long>> dp(n + 1 , vector<long long>(x + 1 , -1));
-    int res = f(0 , x , dp , c);
-    cout << res << endl;
-    return 0;
+    cout << dp[x] << endl;
 }
